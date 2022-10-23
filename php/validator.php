8ff8cac2918151a1454bed89178308808feb1f04
Rafael <rafael2mcont@gmail.com>
2022-10-22 22:09:30 -0300

    Integration of PHP, HTML and onclick comand of Javasript with anythings screen of Lucas

--- php/validator.php
+++ php/validator.php
@@ -1,19 +1,22 @@
 <?php
-    $email = isset($_POST["email_input"])?$_POST["email_input"]:"Nao informado";
-    $password = isset($_POST["pw_input"])?$_POST["pw_input"]:"Nao informado";
+    // $btn_login = isset($_GET["btn_login"])?$_GET["btn_login"]:"Nao informado";
+    // $btn_forget = isset($_GET["btn_forget"])?$_GET["btn_forget"]:"Nao informado";
+    $no_info = 'Nao informado';
+    $email = isset($_POST["email"])?$_POST["email"]:$no_info;
+    $password = isset($_POST["password"])?$_POST["password"]:$no_info;
 
-    //condições temporárias até obtermos o BD do NTI ou acharmos uma alternativa pra não integração
-    if (strcmp($email, "servidor@ic.ufal.br")==0){
-        // window.location.href = "https://admin.html";
-        header("Location: ../admin.html");
+    // condições temporárias até obtermos o BD do NTI ou acharmos uma alternativa pra não integração
+    // echo "btn_new: " . $btn_login;
+    // echo "btn_new: " . $btn_forget;
+    $valid = 'servidora@ic.ufal.br';
+    // echo "email" . $email;
+    if (strcmp($email, $valid)==0){
+        header("Location: ../disciplines.html");
     }
-    // else if (strcmp($email, "discente@ic.ufal.br")==0){
-
-    // }
     else{
-        echo "Erro ao logar!";
-        header("Location:https://index.html");
+        header("Location: ../index.html");
     }
+    
     exit();
     die();
 ?>
